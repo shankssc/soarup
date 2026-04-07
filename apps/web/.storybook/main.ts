4d9b897dc9e147ee5955b708b618698704931e69
shankssc22 <suyash@athleda.org>
2026-04-06 21:04:39 -0400

    chore: added supabase cli and fixed docker-compose

--- apps/web/.storybook/main.ts
+++ apps/web/.storybook/main.ts
@@ -1,20 +1,32 @@
-import type { StorybookConfig } from '@storybook/nextjs-vite';
+import type { StorybookConfig } from "@storybook/nextjs-vite";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  stories: [
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
-  "addons": [
-    "@chromatic-com/storybook",
-    "@storybook/addon-vitest",
-    "@storybook/addon-a11y",
+
+  addons: [
     "@storybook/addon-docs",
-    "@storybook/addon-onboarding"
+    "@storybook/addon-a11y",
   ],
-  "framework": "@storybook/nextjs-vite",
-  "staticDirs": [
-    "..\\public"
-  ]
+
+  framework: "@storybook/nextjs-vite",
+
+  staticDirs: ["..\\public"],
+
+  viteFinal: async (config) => {
+    if (config.resolve) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        "@": path.resolve(__dirname, "../src"),
+      };
+    }
+    return config;
+  },
 };
+
 export default config;
